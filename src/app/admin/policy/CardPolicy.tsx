28dfb0e72ba55096a6c7517c9267617444d8805d
LeHaiDang <0509dangle@gmail.com>
2024-04-19 00:01:52 +0700

    s

--- src/app/admin/policy/CardPolicy.tsx
+++ src/app/admin/policy/CardPolicy.tsx
@@ -11,8 +11,9 @@ import {
   DialogFooter,
   Tooltip,
   Textarea,
+  Alert,
 } from "@material-tailwind/react";
-import { APIUpdatePolicy } from "@/services/Policy";
+import { APIRemovePolicy, APIUpdatePolicy } from "@/services/Policy";
 import Toast from "@/utils/Toast";
 
 interface CardPolicyProps {
@@ -28,15 +29,34 @@ function CardPolicy(props: CardPolicyProps) {
   const { data, handleEdit } = props;
   const [open, setOpen] = React.useState(false);
   const [openDialog, setOpenDialog] = React.useState(false);
+  const [openDialogDel, setOpenDialogDel] = React.useState(false);
   const toggleOpen = () => setOpen((cur) => !cur);
   const handleOpen = () => setOpenDialog((cur) => !cur);
+  const handleOpenDel = () => setOpenDialogDel((cur) => !cur);
   const [content, setContent] = React.useState<string[]>([]);
   const [valueChange, setValueChange] = React.useState<any>();
+  const [alert, setAlert] = React.useState({
+    open: false,
+    message: "",
+  });
   React.useEffect(() => {
     setContent(data.content.split("\n"));
     setValueChange(data);
   }, [data._id]);
   const Edit = async () => {
+    if (valueChange.content.trim() == "") {
+      setAlert({
+        open: true,
+        message: "Nội dung không được để trống",
+      });
+      setTimeout(() => {
+        setAlert({
+          open: false,
+          message: "",
+        });
+      }, 2000);
+      return;
+    }
     const res = await APIUpdatePolicy(valueChange._id, valueChange.content);
     handleOpen();
     if (res.status == 200 || res.status == 201) {
@@ -55,6 +75,17 @@ function CardPolicy(props: CardPolicyProps) {
       Toast("error", res.data.message, 2000);
     }
   };
+
+  const Del = () => {
+    const res = APIRemovePolicy(data._id);
+    // if (res) {
+    //   Toast("success", res.data.message, 2000);
+    //   handleOpenDel();
+    // } else {
+    //   Toast("error", res.data.message, 2000);
+    // }
+  };
+
   return (
     <div className="w-1/3 py-2 pr-3">
       <Tooltip
@@ -83,7 +114,7 @@ function CardPolicy(props: CardPolicyProps) {
               <Button
                 color="red"
                 className="w-2/3 mt-2 ml-2"
-                onClick={handleOpen}
+                onClick={handleOpenDel}
               >
                 Xoá
               </Button>
@@ -94,6 +125,11 @@ function CardPolicy(props: CardPolicyProps) {
       <Dialog open={openDialog} handler={handleOpen} size="lg">
         <DialogHeader>{valueChange?.name}</DialogHeader>
         <DialogBody>
+          {alert.open && (
+            <Alert color="red" className="mb-4">
+              {alert.message}
+            </Alert>
+          )}
           <Textarea
             variant="standard"
             label=""
@@ -111,13 +147,38 @@ function CardPolicy(props: CardPolicyProps) {
           <Button
             variant="text"
             color="red"
-            onClick={handleOpen}
+            onClick={(e) => {
+              setValueChange(data);
+              handleOpen();
+            }}
             className="mr-1"
           >
             <span>Đóng</span>
           </Button>
           <Button variant="gradient" color="yellow" onClick={(e) => Edit()}>
             <span>Chỉnh sửa</span>
+          </Button>
+        </DialogFooter>
+      </Dialog>
+      <Dialog open={openDialogDel} handler={handleOpenDel} size="md">
+        <DialogHeader>Xoá chính sách</DialogHeader>
+        <DialogBody>
+          <Typography>
+            Bạn có chắc chắn muốn xoá chính sách{" "}
+            <span className="font-bold italic">"{data.name}"</span> không?
+          </Typography>
+        </DialogBody>
+        <DialogFooter>
+          <Button
+            variant="text"
+            color="red"
+            onClick={handleOpenDel}
+            className="mr-1"
+          >
+            <span>Đóng</span>
+          </Button>
+          <Button variant="gradient" color="red" onClick={(e) => Del()}>
+            <span>Xoá</span>
           </Button>
         </DialogFooter>
       </Dialog>
--- src/services/Policy.tsx
+++ src/services/Policy.tsx
@@ -22,3 +22,25 @@ export const APIUpdatePolicy = async (id: string, content: string) => {
   console.log(res);
   return res;
 };
+
+// /api/policies
+export const APIAddPolicy = async (policy: any) => {
+  const headers = GetHeaders();
+  const res = await axios.post(
+    `${process.env.NEXT_PUBLIC_API_URL}/policies`,
+    policy,
+    { headers }
+  );
+  return res;
+};
+
+// /api/policies/65ddf631c4500250d92b147e
+export const APIRemovePolicy = async (id: string) => {
+  const headers = GetHeaders();
+  const res = await axios.delete(
+    `${process.env.NEXT_PUBLIC_API_URL}/policies/${id}`,
+    { headers }
+  );
+  console.log("APIRemovePolicy", res);
+  return res;
+};
